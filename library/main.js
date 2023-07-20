f6d698753afefc19cca744264935311bb84045a6
wilberforce001 <kenmisik@gmail.com>
2023-07-20 12:16:18 +0300

    Set the checkboxes if book is read or unread

--- library/main.js
+++ library/main.js
@@ -20,11 +20,37 @@ function createBookElement(el, content, className) {
     return element;
 }
 
+function createReadElement(bookItem, book) {
+    const read = document.createElement("div");
+    read.setAttribute("class", "book-read");
+    read.appendChild(createBookElement("h1", "Read?", "book-read-title"));
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.addEventListener('click', (e) => {
+        if(e.target.checked) {
+            bookItem.setAttribute('class', "card book read-checked");
+            book.read =true;
+    
+        } else {
+            bookItem.setAttribute("class", "card book read-unchecked");
+            book.read = false;
+               
+        }
+    });
+    if(book.read) {
+        input.checked = true;
+        bookItem.setAttribute("class", "card book read-checked");
+    }
+    read.appendChild(input);
+    return read;
+
+}
+
 function createBookItem (book, index) {
     const bookItem = document.createElement('div');
-    bookItem.setAttribute('id', index)
-    bookItem.setAttribute('key', index)
-    bookItem.setAttribute('class', 'card book');
+    bookItem.setAttribute('id', index);
+    bookItem.setAttribute('key', index);
+    bookItem.setAttribute("class", "card book");
     bookItem.appendChild(
         createBookElement("h1", `Title: ${book.title}`, "book-title")
     );
@@ -34,13 +60,15 @@ function createBookItem (book, index) {
     bookItem.appendChild(
         createBookElement("h1", `Pages: ${book.pages}`, "book-pages")
     );
+    bookItem.appendChild(createReadElement(bookItem, book));
+
     books.insertAdjacentElement("afterbegin", bookItem);
 }
 
 function renderBooks () {
     myLibrary.map((book, index) => {
-        createBookItem(book, index)
-    })
+        createBookItem(book, index);
+    });
 }
 
 renderBooks();
