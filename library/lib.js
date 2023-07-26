13393c49f55a24d01b55658873d6ae3bbb133f4c
wilberforce001 <kenmisik@gmail.com>
2023-07-26 15:41:20 +0300

    Finally been able to correctly render the svgs and locate editIcon in the rightful place

--- library/lib.js
+++ library/lib.js
@@ -64,7 +64,10 @@ function createIcon3() {
 function createEditIcon(book) {
   const editIconPathData = "M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z";
 
-  return createIcons(editIconPathData, "24", "24");
+  const editIcon = createIcons(editIconPathData, "24", "24");
+  editIcon.setAttribute("class", "edit-icon"); // Add class to the SVG element
+
+  return editIcon;
 }
 
 function createIcons(pathData, width = "20", height = "20") {
@@ -105,9 +108,11 @@ function createBookItem(book, index) {
     bookItem.appendChild(createReadElement(bookItem, book));
     bookItem.appendChild(createBookElement("button", "X", "delete"));
     const icon1 = createIcon1();
-    const editIcon = createEditIcon(book);
     const icon2 = createIcon2 ();
     const icon3  = createIcon3 ();
+    const editIcon = createEditIcon(book);
+
+
     bookItem.appendChild(icon1);
     bookItem.appendChild(icon2);
     bookItem.appendChild(icon3);
